6e687e6102e1e032d0e63d9524c2b5c3891eb245
ST10434285 <st10434285@vcconnect.EDU.ZA>
2024-10-22 11:40:52 +0200

    inital commit

---
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.practiceapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var NameTextView = findViewById<TextView>(R.id.NameTextView)
+        val EnterButton = findViewById<Button>(R.id.EnterButton)
+
+
+
+        EnterButton.setOnClickListener {
+            val intent = Intent(this, Weather::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
